27bdb7449cbadc2da85a19240a633857a2f4ff52
Brett Jurman <i.be.brett@gmail.com>
2025-02-18 09:16:07 -0500

    adding initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='owls_compress',
+    version='1.0',
+    description='An experiment in compression',
+    author='SCSU CS Club',
+    packages=['owls_compress'],
+)
+
+
